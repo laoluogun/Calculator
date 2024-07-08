f3bd421802663c98efd2b83b68c4cf3665171a08
femi-anmly <femiogun9590@gmail.com>
2024-07-08 11:04:48 -0400

    Create four basic mathematical functions

---
+++ script.js
@@ -0,0 +1,20 @@
+const addNums = function(num1, num2) {
+    return num1 + num2;
+}
+
+const subtractNums = function(num1, num2) {
+    return num1 - num2;
+}
+
+const multiplyNums = function(num1, num2) {
+    return num1 * num2;
+}
+
+const divideNums = function(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(addNums(8,4));
+console.log(subtractNums(8,4));
+console.log(multiplyNums(8,4));
+console.log(divideNums(8,4));
